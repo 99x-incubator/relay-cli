149c7ca09d79a246ed626c1f0c90fda277feae37
Rifhan Akram <rifhan.akram1@gmail.com>
2016-10-22 04:40:57 +0530

    - Add feature to generate containers

--- bin/init.js
+++ bin/init.js
@@ -5,6 +5,7 @@ const inquirer = require('inquirer');
 const initApp = require('../lib/initialize');
 const packageVersion = require('../package.json').version;
 const ora = require('ora');
+const containerApp = require('../lib/container');
 
 /**
  * set commander version
@@ -46,6 +47,37 @@ program
     console.log('    $ react-cli init -l awesomereact');
     console.log();
   });
+
+
+
+  program
+  .command('generate [type] [module] [name]')
+  .alias('g')
+  .description('generate relay container')
+  .action(function(type,module,name) {
+    if (type === undefined) {
+      console.log('provide a container name');
+      return;
+    }
+    else
+		{
+      container = new containerApp()
+      container.createContainer(type,module,function(result){
+        if(result) {
+          const spinner = ora(`creating ${type} ${name}`).start();
+          spinner.text = `${type} ${module} created`
+          spinner.succeed();
+
+        }  
+      },name)
+		}
+  }).on('--help', function() {
+    console.log('  Examples:');
+    console.log();
+    console.log('    $ relay init awesomereact');
+    console.log('    $ react-cli init -l awesomereact');
+    console.log();
+  }); 
 
 /**
  * parse commander object
